4b2ba7ef071c53540920efaab9d4ae93e2da6ed8
barale2906 <alexanderbarajas@gmail.com>
2026-03-13 00:52:17 -0500

    Ajuste produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  /** URL del backend. Ajustar según despliegue (proxy, CORS, etc.) */
-  apiUrl: '',
-  /** WebSockets. Opcional. */
+  apiUrl: 'https://uniph-api.gislasas.com',
   wsUrl: '',
   pusherKey: '',
 };
